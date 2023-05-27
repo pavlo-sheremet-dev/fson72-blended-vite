4940685ce12c4b372bef3282250f2e98965b3386
ZachepaRostislav <zachepa.rostislav@gmail.com>
2023-05-27 20:00:59 +0300

    finish

--- src/views/CocktailDetail.jsx
+++ src/views/CocktailDetail.jsx
@@ -2,13 +2,43 @@ import { Section } from '../components/Section';
 import { Loader } from '../components/Loader';
 import { GoBackBtn } from '../components/GoBackBtn';
 import { CocktailInfo } from '../components/CocktailInfo';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { routes } from '../routes';
+import { useEffect, useRef, useState } from 'react';
+import { getCocktailDetail } from '../api/cocktail-service';
 
 export const CocktailDetail = () => {
+  const { cocktailId } = useParams()
+  const [cocktail, setCocktail] = useState(null)
+  const [isloading, setIsloading] = useState(false)
+  const [error, setError] = useState(null)
+  const location = useLocation()
+  const backLink = useRef(location.state?.from ?? { pathname: '/' })
+
+  useEffect(() => {
+    (async () => {
+      setIsloading(true)
+      try {
+        const data = await getCocktailDetail(cocktailId)
+        setCocktail(data)
+      } catch (error) {
+        setError(error.message)
+      }
+      finally {
+        setIsloading(false)
+      }
+    })()
+  }, [cocktailId])
+
   return (
-    <h1 className='uppercase text-4xl text-gray-600 text-center'>
-      CocktailDetail
-    </h1>
+    <Section>
+      <h1 className='uppercase text-4xl text-gray-600 text-center'>
+        CocktailDetail
+      </h1>
+      < GoBackBtn path={backLink.current} />
+      {cocktail && <CocktailInfo {...cocktail} />}
+      {isloading && <Loader />}
+    </Section>
+
   );
 };
